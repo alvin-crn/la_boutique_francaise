4a3eda19f88a10ca57caa760a9c1f6159f419d15
Alvin CARNEIRO <alvin.carneiro1@gmail.com>
2022-02-08 15:18:24 +0100

    ajout section commandes au tableau de bord administrateur donnant un recap des commande payé ou non sur le site

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Carrier;
 use App\Entity\Category;
+use App\Entity\Order;
 use App\Entity\Product;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,18 +26,19 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
 
-        $url = $this->adminUG
-            ->setController(UserCrudController::class)
-            ->generateUrl();
+        // $url = $this->adminUG
+        //     ->setController(UserCrudController::class)
+        //     ->generateUrl();
 
-        return $this->redirect($url);
+        // return $this->redirect($url);
         
         // return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+            $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+            
+            return $this->redirect($adminUrlGenerator->setController(OrderCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -60,6 +62,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Commandes', 'fas fa-shopping-cart', Order::class);
         yield MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class);
         yield MenuItem::linkToCrud('Produits', 'fas fa-tag', Product::class);
         yield MenuItem::linkToCrud('Transporteurs', 'fas fa-truck', Carrier::class);
